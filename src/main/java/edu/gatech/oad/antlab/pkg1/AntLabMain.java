9d316c0cd4fe7c1204efbd567b40fe738d4ec8bf
rakyu012 <bpark31@gatech.edu>
2014-01-21 12:00:34 -0500

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,12 +61,12 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("902382902");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("gburdell2");
+		  Person2 p2 = new Person2("Bum Jun Park");
+		  toPrint += p2.toString("902627979");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("YongHui Cho");
+		  toPrint += p3.toString("902648145");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Anish Sharma");
